fc66c05a0b7afa73cb6f11bc17d9865d78a355d9
github username <um_2003_8@hotmail.com>
2022-05-03 13:54:07 -0400

    import reducers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import './index.css';
+import reducers from './reducers'
+import './index.css'
 import App from './components/App'
 
 ReactDOM.render(
