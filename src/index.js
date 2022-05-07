f00164769a03fe5554830f07fb0d6e080c10eb2a
github username <um_2003_8@hotmail.com>
2022-05-07 15:43:21 -0400

    remove createStore since it is not in use anymore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
 import App from './components/App'
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store= {store}>
     <App />
   </Provider>,
   document.getElementById('root')
